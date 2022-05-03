f1ae7b32c0aa54577e6fcec31b3b755ec9ee0c18
foxcode11 <97304706+foxcode11@users.noreply.github.com>
2022-05-03 22:06:09 +0530

    Add files via upload

---
+++ headortail.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+void flip()
+{
+    int c=0,r;
+    srand(time(NULL));
+    for(int i=1;i<=100;i++)
+    {
+        r=(rand()%2);
+        if(r==0)
+        {
+            cout<<"TAILS"<<endl;
+            c++;
+        }
+        else
+            cout<<"HEADS"<<endl;
+    }
+    cout<<"THE NUMBER OF TAILS ARE :"<<c<<endl;
+    cout<<"THE NUMBER OF HEADS ARE :"<<(100-c)<<endl;
+
+}
+int main()
+{
+
+    flip();
+    return 0;
+}
